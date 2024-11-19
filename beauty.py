70c10c9a30dfc91322b0035e4d1ea165e7d8b2b8
Caprena1 <robcaprena@gmail.com>
2024-11-19 12:32:57 -0500

    Streamling Columns

--- beauty.py
+++ beauty.py
@@ -6,21 +6,19 @@ extended_cosmetics_df = pd.read_csv('extended_cosmetics_products.csv')
 beauty_products_df = pd.read_csv('beauty_products.csv')
 
 #USING DATAFRAME SLICING-CREATING PYTHON LIST TO PULL NEEDED COLUMNS
-columns_from_ec_df = extended_cosmetics_df[['Product_Name','Brand','Category','Price_USD','Rating','Number_of_Reviews','Skin_Type','Cruelty_Free','Country_of_Origin']]
-columns_from_bp_df = beauty_products_df[['Brand','All_vegan','Black_owned']]
+columns_from_ec_df = extended_cosmetics_df[['Product_Name','Brand','Category','Price_USD','Rating','Number_of_Reviews','Country_of_Origin']]
+columns_from_bp_df = beauty_products_df[['Brand','Black_owned']]
 
-#CLEANING UP THE DATA IN EACH DATASET
-
+#CLEANING UP THE DATA IN EACH DATASET - DROP DUPLICATES 
+unique_brands_ec = columns_from_ec_df.drop_duplicates(subset="Brand")
+unique_brands_bp = columns_from_bp_df.drop_duplicates(subset="Brand")
+unique_productname_ec = columns_from_ec_df.drop_duplicates(subset="Product_Name")
+#combine_columns_ec_bp = [[unique_brands_ec, unique_brands_bp, unique_productname_ec]]
 
 
 #COMBINED NEW DATASET BY USING MERGE(JOIN)
-melanin_newdata = pd.merge(columns_from_ec_df, columns_from_bp_df, left_on='Brand', right_on='Brand')
+melanin_newdata = pd.merge(unique_brands_ec, unique_brands_bp, left_on='Brand', right_on='Brand')
+#melanin_newdata_all = pd.concat(melanin_newdata,columns_from_bp_df, axis=1)
 
-#columns_from_ec_df.columns = columns_from_ec_df.columns.str.strip()
-#columns_from_bp_df.columns = columns_from_bp_df.columns.str.strip()
-
-
-
-#print(columns_from_ec_df)
 print(melanin_newdata)
 
