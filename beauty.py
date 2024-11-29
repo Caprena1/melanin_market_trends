f5817fe9bbf539f77097e04f93660e3fdcb4aa22
Caprena1 <robcaprena@gmail.com>
2024-11-29 16:57:50 -0500

    Fixing Revenue Totals and adding functional visualizations

--- beauty.py
+++ beauty.py
@@ -9,23 +9,16 @@ beauty_products_df = pd.read_csv('beauty_products.csv')
 columns_from_ec_df = extended_cosmetics_df[['Product_Name','Brand','Category','Price_USD','Rating','Number_of_Reviews','Country_of_Origin']]
 columns_from_bp_df = beauty_products_df[['Brand','Black_owned']]
 
-#CLEANING UP THE DATA IN EACH DATASET - DROP DUPLICATES 
-#unique_brands_ec = columns_from_ec_df.drop_duplicates(subset="Brand")
-#unique_brands_bp = columns_from_bp_df.drop_duplicates(subset="Brand")
-#unique_productname_ec = columns_from_ec_df.drop_duplicates(subset="Product_Name")
-
-
 #COMBINED NEW DATASET BY USING MERGE(LEFT JOIN)-if how='left' is not used, it's an inner join
 melanin_newdata = pd.merge(columns_from_ec_df, columns_from_bp_df, how='left', left_on='Brand', right_on='Brand')
-melanin_newdata['Black_owned'] = melanin_newdata['Black_owned'].fillna('True')
+melanin_newdata['Black_owned'] = melanin_newdata['Black_owned'].fillna(True)
 
 #FILTERING THE DATA TO CALCULATE NEW VALUES
-revenue = melanin_newdata[melanin_newdata['Price_USD'] == True]
 black_owned = melanin_newdata[melanin_newdata['Black_owned'] == True]
 non_black_owned = melanin_newdata[melanin_newdata['Black_owned'] == False]
 
-#TOTAL REVENUE
-all_revenue = revenue['Price_USD'].sum()
+#TOTAL REVENUE AND TOTAL REVENUE GROUPED
+all_revenue = melanin_newdata['Price_USD'].sum()
 black_owned_revenue = black_owned['Price_USD'].sum()
 non_black_owned_revenue = non_black_owned['Price_USD'].sum()
 
@@ -47,18 +40,35 @@ print("\nRevenue by category for non_Black-owned companies:")
 print(non_black_owned_grouped)
 
 #PREPARING DATA FOR VISUALIZATION USING MATPLOTLIB
-comparison = melanin_newdata.groupby('Black_owned')['Price_USD'].sum()
-comparison.index = ['Non-Black-Owned', 'Black-Owned'] #Renamed for clarity
+comparison = pd.Series([100, 200], index=['Non-Black', 'Black-Owned'])
 
-#PLOT
-comparison.plot(kind='bar', color=['orange', 'blue'], title='Revenue Comparison')
-plt.ylabel('Total Revenue')
-plt.show
+# #PLOT - BAR
+comparison.plot(kind='bar', color=['orange', 'blue'], title='Revenue Comparison-Bar')
+plt.ylabel('Revenue Comparison 1')
+plt.show()
 
-#comparison.plot(kind='bar', color=['red', 'yellow'], title='Revenue Comparison')
-#plt.ylabel('Total Revenue')
-#plt.show
+#PLOT - PIE
+comparison.plot(kind='pie', colors=['red', 'yellow'], title='Revenue Comparison-Pie', autopct='%1.1f%%')
+plt.ylabel('') #Removes y-axis label for cleaner aesthetics
+plt.show()
 
-#comparison.plot(kind='sine', color=['orange', 'blue'], title='Revenue Comparison')
-#plt.ylabel('Total Revenue')
-#plt.show
+#PLOT - BOX 
+plt.boxplot(comparison)
+plt.title('Revenue Comparison - Box Plot')
+plt.ylabel('Revenue Comparison 3')
+plt.show()
+#PLOT - SCATTER
+plt.scatter(comparison.index, comparison.values, color='green', label='Revenue Comparison-Scatter')
+plt.legend()
+plt.show()
+
+#PLOT - SINE WAVE
+x = np.linspace(0,2 * np.pi, 100)
+y = np.sin(x)
+plt.plot(x, y, color='green', label='Revenue Comparison Sine Wave')
+plt.legend()
+plt.show()
+
+
+
+
